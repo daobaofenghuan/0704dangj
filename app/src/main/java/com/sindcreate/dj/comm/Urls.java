36a6d75270b44831665491e0bd872ce0e706bbad
Double <530070626@qq.com>
2018-07-08 13:37:39 +0800

    url

---
+++ app/src/main/java/com/sindcreate/dj/comm/Urls.java
@@ -0,0 +1,27 @@
+package com.sindcreate.dj.comm;
+
+/**
+ * Created by qlw on 2018/5/7.
+ */
+
+public class Urls {
+    public final static String BASE_URL="";
+    public final static String UsrLogin="/api/auth/login";
+    public final static String REGISTER="register";
+
+//三会一课
+    public final static String SANHUI_ING="/api/partyAffiars/queryAffairsForThree/02";
+    public final static String SANHUI_END="/api/partyAffiars/queryAffairsForThree/03";
+    //党务工作
+    public final static String DANGWU_ING="/api/partyAffiars/queryAffairsNotForTree/02";
+    public final static String DANGWU_END="/api/partyAffiars/queryAffairsNotForTree/03";
+    public final static String DANGWU_INDEX="/api/partyAffiars/detailForMobile";
+
+
+    public final static String NEWS_URL="/api/news/queryforlist";
+
+
+
+
+
+}
